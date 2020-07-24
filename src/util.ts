772a0c97cd71d9eda25bf0c3692638e11b55c0a6
Thomas Röggla <tom@lichtfragmente.com>
2020-07-25 01:43:12 +0200

    set compilation target to ES6 and removed padStart function

--- src/util.ts
+++ src/util.ts
@@ -1,33 +1,4 @@
 /**
- * Pads the given string to the left with the given pad character until the pad
- * lenght is reached. If a string that is equal or longer to the target length
- * is passed in, the string is returned unchanged.
- *
- * @param str String to pad
- * @param targetLen Target length
- * @param pad Pad character
- * @returns The padded string
- */
-function padStart(str: string, targetLen: number, pad: string): string {
-  // Return string if it is longer or equal to targetLen
-  if (str.length >= targetLen) {
-    return str;
-  }
-
-  // Initialise padding array to required length
-  const padLen = targetLen - str.length;
-  const padding: Array<string> = [];
-
-  // Generate array with padding character
-  for (let i=0; i<padLen; i++) {
-    padding[i] = pad;
-  }
-
-  // Join array, concatenate with input and return
-  return padding.join("") + str;
-}
-
-/**
  * Takes a number, converts it to binary and makes sure it is padded to a
  * length of 8 (i.e. a byte). If the input binary is longer than 8, it is
  * returned unchanged.
@@ -36,7 +7,7 @@ function padStart(str: string, targetLen: number, pad: string): string {
  * @returns Binary number as a string of length 8
  */
 export function toPaddedBinary(num: number): string {
-  return padStart(num.toString(2), 8, "0");
+  return num.toString(2).padStart(8, "0");
 }
 
 /**
